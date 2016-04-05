f474ac108406e358e2a60c8a3020aac1d2c6b877
John Skilbeck <john.skilbeck@gmail.com>
2016-04-05 16:16:09 -0700

    python

--- grep.py
+++ grep.py
@@ -1,13 +1,25 @@
 
 
 shakeseare = 'pg100.txt'
+out = {}
 
 def extract(file):
 	for line in file:
 		strip()
 
 def strip(arr):
-	arr.foreach
+	arr(lambda x: 
+		x.replace(/[^a-zA-Z ]/g,'') 
+		add(x)
+	)
+
+def add(word):
+	if !out[word]:
+		out[word] = 1
+	else:
+		out[word] += out[word]
 
 with f as open():
 	extract(f)
+
+print(out)
