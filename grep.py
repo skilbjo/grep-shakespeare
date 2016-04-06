0479607db9e82de77d863532d6332942829f4286
John Skilbeck <john.skilbeck@gmail.com>
2016-04-06 10:45:27 -0700

    grep in python

--- grep.py
+++ grep.py
@@ -1,25 +1,46 @@
+import re
 
-
-shakeseare = 'pg100.txt'
-out = {}
+shakeseare = './data/pg100.txt'
+out = dict()
 
 def extract(file):
 	for line in file:
-		strip()
+		transform(line)
+	file.close()
 
-def strip(arr):
-	arr(lambda x: 
-		x.replace(/[^a-zA-Z ]/g,'') 
-		add(x)
-	)
+def transform(line):
+	words = line.split()
+	words = list(map(lambda x: strip(x), words))
+	load(words)
+
+def load(row):
+	map(lambda x: add(x), row)
 
 def add(word):
-	if !out[word]:
-		out[word] = 1
+	print(word)
+	if word in out:
+		out[word] += 1
 	else:
-		out[word] += out[word]
+		out[word] = 0
+	  # out[word] = out.get(word, 0) + 1
+	# if !out[word]:
+	# 	out[word] = 1
+	# else:
+	# 	out[word] += out[word]
 
-with f as open():
-	extract(f)
+def strip(word):
+	return re.sub('[^A-Za-z]', '', word.lower())
 
-print(out)
+def open_file(file):
+	with open(file,'r') as f:
+		extract(f)
+
+open_file(shakeseare)
+
+# def add(word):
+# 	if !out[word]:
+# 		out[word] = 1
+# 	else:
+# 		out[word] += out[word]
+
+# print(out)
---
+++ py.py
@@ -0,0 +1,5 @@
+items = [1,2,3,4,5]
+
+def sqr(x): return x**2
+
+print list(map(sqr,items))
