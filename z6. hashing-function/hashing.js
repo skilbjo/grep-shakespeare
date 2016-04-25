592932bfb740723e8eed8115d9302ea613c2cd4d
John Skilbeck <john.skilbeck@gmail.com>
2016-04-25 11:04:54 -0700

    hashing function

---
+++ z6. hashing-function/hashing.js
@@ -0,0 +1,31 @@
+
+var data_store = [ ],
+	buckets = 4,
+	node = { key: value, next: undefined }
+	;
+
+var hash = function(input, data_store){
+	var bucket = input % buckets;
+
+};
+
+
+var hash_lookup = function( target, data_store ){
+	var bucket = hash(target,data_store);
+
+	for(var key in data_store[bucket]){
+		if(data_store[bucket].hasOwnProperty(key){ // key:key, data_store[bucket][key]:value
+			
+		})
+	}
+
+	data_store[bucket].map(function(node,iterator){
+
+	})
+	while(next.value){
+		if(key == target)
+			return 
+	}
+
+};
+
