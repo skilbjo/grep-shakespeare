6ce7805f944f136788a3a50e315ad7700ae679e6
John Skilbeck <john.skilbeck@gmail.com>
2016-04-28 21:51:12 -0700

    hard problem

---
+++ z8. multiple-eval/mutlple-eval.js
@@ -0,0 +1,26 @@
+/* note - the runtime on this is ridiculous */
+
+var calc = function(expression,input,target){
+	if(input.length == 1){
+		if( new Function('return '+expression+input)() == target ){
+			console.log(expression+input+' = '+target);
+		}
+	} else {
+		for(var i = 1; i <= input.length; i++){
+			var current = input.substring(0,i),
+				remaining = input.substring(i);
+			['+','-','/','*'].map(function(operator){
+				calc(expression + current + operator, remaining, target);
+			});
+		}
+	}
+};
+
+var f = function(sequence, target){
+	calc('',sequence,target);
+};
+
+var testInput1 = f('314159265358',27182)
+	;
+
+console.log(testInput1);
