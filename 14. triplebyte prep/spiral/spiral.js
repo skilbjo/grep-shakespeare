8b50fad3c6380350e04ae9a4e84109de0918f6d3
John Skilbeck <john.skilbeck@gmail.com>
2016-05-23 18:33:57 -0700

    refactored

--- 14. triplebyte prep/spiral/spiral.js
+++ 14. triplebyte prep/spiral/spiral.js
@@ -1,27 +1,12 @@
 var mark = '*',
-	n = 8,
-	create_arr = function(n){
-		var arr = [];
-		for(var row=0; row < n; row++){
-			var line = [];
-			for(var field=0; field < n; field++){
-				line.push(' ');
-			}
-			arr.push(line);
-		}
-		return arr;
+	n = 25,
+	create_arr = function(n){ var arr = []; for(var row=0; row < n; row++){var line = [];
+			for(var field=0; field < n; field++){	line.push(' '); }	arr.push(line);	}	return arr;
 	},
 	arr = create_arr(n),
-	textify = function(){
-	var result = '';
-	for(var row=0; row<= arr.length-1; row++){
-		for(var col=0; col<= arr.length-1; col++){
-			result+=arr[row][col];
-		}
-		result+='\n';
-	}
-	return result;
-};
+	textify = function(){	var result = ''; for(var row=0; row<= arr.length-1; row++){
+		for(var col=0; col<= arr.length-1; col++){	result+=arr[row][col];	}	result+='\n';	}	return result;
+	};
 
 var draw_at_position = function(row,col){
 	arr[row][col] = mark;
@@ -31,19 +16,19 @@ var draw_at_position = function(row,col){
 var draw_line = function(start,len,dir){
 	var position = start;
 	switch(dir){
-		case 'L': for(var col = start[1]; col >= (start[1] - len +1); col--){
+		case 'L': for(var col = start[1]; col >= (start[0] - len); col--){
 			draw_at_position(start[0],col);
-			position = [start[0],col];
-			} break;
-		case 'R': for(var col = start[1]; col <= (len); col++){
+			position = [start[0]-1,col];
+			} break; // col = 2; col <= 6 (col = 4)
+		case 'R': for(var col = start[1]; col <= (len + start[1]); col++){
 				draw_at_position(start[0],col);
 				position = [start[0],col];
 			} break;
-		case 'U': for(var row = start[0]; row >= (start[0] - len +1); row--){
+		case 'U': for(var row = start[0]; row >= (start[0] - len + 1); row--){
 			draw_at_position(row,start[1]);
 			position = [row,start[1]];
 			} break;
-		case 'D': for(var row = start[0]; row <= (len); row++){
+		case 'D': for(var row = start[0]; row <= (start[0] + len); row++){
 				draw_at_position(row,start[1]);
 				position = [row,start[1]];
 			} break;				
@@ -51,35 +36,36 @@ var draw_line = function(start,len,dir){
 	return position;
 };
 
-var spiral = function(n){
-	var counter = n - 1;
-	var next = draw_line([0,0],arr.length-1,'R'),
-		order = ['R','U','L','D'],
-		directions = [];
+var directions_stack = function(n){
+	var order = ['R','U','L','D'],
+		directions = []
+		;
 
-	for(var i = 0; i<= counter; i++){
+	for(var i = 0; i <= n-1; i++){
 		var tmp = '';
 		tmp = order.pop();
 		directions.unshift(tmp);
 		order.unshift(tmp);
 	}
-
-	while(counter >= 0){
-		next = draw_line( next , counter, directions.pop() );
-		counter--;
-	}
-	draw_line( next , counter+2, directions.pop() );	
+	return directions;
 };
 
+var spiral = function(n){
+	var next = draw_line([0,0],arr.length-1,'R'),
+		directions = directions_stack(n);
+
+	for(var i = n - 1; i >= 0; i--){
+		next = draw_line( next , i, directions.pop() );
+	}
+};
 
 var f = function(n){
-	console.log('function f called with parameter: ',n);
-	console.log('answer:');
+	// console.log('function f called with parameter: ',n);
+	// console.log('answer:');
 	spiral(n);
+	// console.log(arr);
 	console.log(textify());
 };
 
 f(n);
-
-
 
--- 14. triplebyte prep/spiral/spiral3.js
+++ 14. triplebyte prep/spiral/spiral3.js
@@ -1,5 +1,5 @@
 var mark = '*',
-	n = 7,
+	n = 8,
 	create_arr = function(n){
 		var arr = [];
 		for(var row=0; row < n; row++){
@@ -31,19 +31,19 @@ var draw_at_position = function(row,col){
 var draw_line = function(start,len,dir){
 	var position = start;
 	switch(dir){
-		case 'L': for(var col = start[1]; col >= (start[0] - len); col--){
+		case 'L': for(var col = start[1]; col >= (start[1] - len +1); col--){
 			draw_at_position(start[0],col);
-			position = [start[0]-1,col];
-			} break; // col = 2; col <= 6 (col = 4)
-		case 'R': for(var col = start[1]; col <= (len + start[1]); col++){
+			position = [start[0],col];
+			} break;
+		case 'R': for(var col = start[1]; col <= (len); col++){
 				draw_at_position(start[0],col);
 				position = [start[0],col];
 			} break;
-		case 'U': for(var row = start[0]; row >= (start[0] - len + 1); row--){
+		case 'U': for(var row = start[0]; row >= (start[0] - len +1); row--){
 			draw_at_position(row,start[1]);
 			position = [row,start[1]];
 			} break;
-		case 'D': for(var row = start[0]; row <= (start[0] + len); row++){
+		case 'D': for(var row = start[0]; row <= (len); row++){
 				draw_at_position(row,start[1]);
 				position = [row,start[1]];
 			} break;				
@@ -52,21 +52,23 @@ var draw_line = function(start,len,dir){
 };
 
 var spiral = function(n){
+	var counter = n - 1;
 	var next = draw_line([0,0],arr.length-1,'R'),
 		order = ['R','U','L','D'],
 		directions = [];
 
-	for(var j = 0; j <= n-1; j++){
+	for(var i = 0; i<= counter; i++){
 		var tmp = '';
 		tmp = order.pop();
 		directions.unshift(tmp);
 		order.unshift(tmp);
 	}
 
-	for(var i = n - 1; i >= 0; i--){
-		var direction = directions.pop();
-		next = draw_line( next , i, direction );
+	while(counter >= 0){
+		next = draw_line( next , counter, directions.pop() );
+		counter--;
 	}
+	draw_line( next , counter+2, directions.pop() );	
 };
 
 
@@ -74,7 +76,6 @@ var f = function(n){
 	console.log('function f called with parameter: ',n);
 	console.log('answer:');
 	spiral(n);
-	console.log(arr);
 	console.log(textify());
 };
 
