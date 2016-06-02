2856a9e0f5822a1cee1c64b3d3d8713dbd500761
skilbjo <john.skilbeck@gmail.com>
2016-06-01 22:24:52 -0700

    set 1

--- 15. crypto pals/set1.1.py
+++ 15. crypto pals/set1.1.py
@@ -1,14 +1,23 @@
-# Goal - convert hex to base64
-string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-
-result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+import base64
 
 def convert_hex_base64(string):
-	return string
+	hex = bytes.fromhex(string)	#.decode('utf-8')
+	base_sixfour = base64.b64encode(hex)
+	return base_sixfour.decode()
 
-
-if __name__ == '__main__':
-	if ( convert_hex_base64(string) == result):
+def test(fn,goal,target):
+	if( fn(goal) == target):
 		print('passed!')
 	else:
 		print('keep trying')
+
+def main():
+	# Goal - convert hex to base64
+	string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+	result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+	test(convert_hex_base64,string,result)
+
+
+if __name__ == '__main__':
+	main()
